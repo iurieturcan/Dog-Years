6fd54748ff4fa212371e78efdab6fa55b5f58afd
Iurie Turcan <iutscan1989@gmail.com>
2021-04-29 21:07:35 +0300

    Dog Years Project

---
+++ Dog Years.js
@@ -0,0 +1,24 @@
+const myAge = 32 ;
+//
+let earlyYears = 2 ;
+//
+earlyYears = earlyYears *= 10.5 ;
+
+let laterYears = myAge / 2 ;
+//
+laterYears *= 4 ; 
+//
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeinDogYears = earlyYears + laterYears ; 
+
+const myName = 'Iurie'.toLowerCase();
+
+//
+console.log(`My Name is ${myName}. I am ${myAge} years old in humnan years which in ${myAgeinDogYears} old in dog years`);
+//
+
+
+
+
