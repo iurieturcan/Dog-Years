f681cb35e39ceb6d6eb6b8471da04a1e09a9affc
Iurie Turcan <iutscan1989@gmail.com>
2021-04-29 21:18:56 +0300

    Dog Years

--- Dog Years.js
+++ Dog Years.js
@@ -1,23 +1,24 @@
 const myAge = 32 ;
-//
+//Constat variable which indicates my Age 
 let earlyYears = 2 ;
-//
-earlyYears = earlyYears *= 10.5 ;
+//Settig a variable to know the early dog years.
+earlyYears *= 10.5 ;
 
 let laterYears = myAge / 2 ;
-//
+//Settig a variable to know the later dog years.
 laterYears *= 4 ; 
-//
+
+//Checking the early years and later dog years
 console.log(earlyYears);
 console.log(laterYears);
 
 let myAgeinDogYears = earlyYears + laterYears ; 
 
 const myName = 'Iurie'.toLowerCase();
+// A varaible for my name transformed in Lower Case.
 
-//
 console.log(`My Name is ${myName}. I am ${myAge} years old in humnan years which in ${myAgeinDogYears} old in dog years`);
-//
+//Creating the console in result to know the dog years comparing to mine. 
 
 
 
